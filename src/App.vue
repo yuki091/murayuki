4e384d0cfcbab2ad449148254de8ba0eccb6a9ab
村上勇希 <murayuki@murakamiisamunozominoMacBook-Air.local>
2020-06-07 15:03:50 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,40 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html
+, body , div , span , object , iframe
+h1
+, h2 , h3 , h4 , h5 , h6 , p , blockquote ,
+pre
+abbr
+, address , cite , code
+del
+, dfn , em , img , ins , kbd , q , samp
+small
+, strong , sub , sup , var
+b
+, i
+dl
+, dt , dd , ol , ul , li
+fieldset
+, form , label , legend
+table
+, caption , tbody , tfoot , thead , tr ,
+th , td
+article
+, aside , canvas , details ,
+figcaption , figure
+footer
+, header , hgroup , menu , nav ,
+section , summary
+time
+, mark , audio , video {
+margin: 0;
+padding: 0;
+border: 0;
+outline: 0;
+font-size:100%;
+vertical-align:baseline;
+background:transparent;
 }
 </style>
---
+++ src/views/header.vue
@@ -0,0 +1,86 @@
+<template>
+  <header>
+    <div class="flex between">
+      <h1 class="header-logo">Logo</h1>
+      <nav class="header-nav">
+        <ul>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Acsece</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  height: 100px;
+  background: #ffffff;
+}
+.page-header {
+  display: flex;
+  justify-content: space-between;
+}
+.header-logo {
+  font-size: 30px;
+  font-weight: bold;
+  margin-left: 35px;
+  color: #000000;
+  line-height: 100px;
+}
+.header-nav li {
+  display: inline-block;
+  margin-right: 60px;
+  font-size: 16px;
+}
+.header-nav a {
+  color: #000000;
+}
+@media screen and (max-width: 768px) {
+  .header-nav {
+    display: none;
+  }
+
+  #hamburger {
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    cursor: pointer;
+    z-index: 100;
+    top: 15px;
+    right: 20px;
+    transition: 0.4s;
+  }
+  #hamburger span {
+    position: absolute;
+    left: 2px;
+    display: block;
+    width: 35px;
+    height: 2px;
+    background-color: #000;
+    transition: 0.３s;
+  }
+
+  #hamburger span:nth-of-type(1) {
+    top: 10px;
+  }
+  #hamburger span:nth-of-type(2) {
+    top: 20px;
+  }
+  #hamburger span:nth-of-type(3) {
+    top: 30px;
+  }
+
+  .menu-content {
+    display: block;
+  }
+  .menu-content.menu-title {
+    margin: 40px 20px;
+    font-size: 24px;
+    color: #333333;
+    text-align: left;
+    font-weight: bold;
+  }
+}
+</style>
